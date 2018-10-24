789296fd74be31f7593d2065aa7f7a88cd7bee8e
mohamad monir <mohamadmonirhamid@gmail.com>
2018-10-24 22:49:01 +0200

    first Js

---
+++ week1/script.js
@@ -0,0 +1,72 @@
+// exersice 1
+console.log("hello world");
+console.log("مرحبا ايها العالم");
+console.log("hallo wereld");
+// exersice 2
+console.log("l'm awsome");
+// exersice 3
+let x;
+console.log('the value of my variable x will be: 10');
+console.log(x);
+x = 10 ;
+console.log('the value of my variable x will be: 10');
+console.log(x);
+// exersice 4 
+let words = "it just string";
+console.log("the value of my string will be:  its just string");
+console.log(words);
+words = "other than the first one";
+console.log('the value of my string will be: the new value will appear');
+console.log(words);      
+//exersice 5
+let z = 7.25;
+console.log(z);
+let a = 7;
+console.log(a);
+if (z>a){
+    console.log("this is the highest value " + z);
+}
+else {
+    console.log("this is the highest value " + a);
+}
+//exersice 6
+let arrCntainers = [""];
+console.log("the value of the array " +arrCntainers);
+let favAnimals =["dog","cat","bird"];
+console.log(favAnimals);
+favAnimals = ["dog","cat","bird","baby big"]
+console.log(favAnimals);
+//exersice 7
+let myString = "hello world";
+console.log(myString);
+console.log(myString.length);
+// exersice 8
+let num = 3;
+let string = "hey there";
+let lang = ["javascript","css","html"];
+let q ;
+let w ;
+console.log(typeof num);
+console.log(typeof string);
+console.log(typeof lang) ;
+console.log(typeof q );
+if (q === w){
+    console.log("same type");
+}
+//exersice 9
+let m = 7;
+console.log(m = m % 3);
+let n = 9;
+console.log(n = n % 3);
+let b = 8;
+console.log(b = b % 2);
+// exersice 10 
+let last = ["string",3,0];
+console.log(last);
+let num1 = 6;
+let num2 = 10
+if (num1/0 === num2/0 ){
+    console.log("same type");
+}
+
+
